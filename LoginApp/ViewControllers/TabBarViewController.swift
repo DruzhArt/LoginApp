a31437fbc5c24fbb80de5faec61cf29dec189329
DruzhArt <druzhart90@gmail.com>
2022-10-09 15:06:36 +0300

    a lot of setup taper and so on

---
+++ LoginApp/ViewControllers/TabBarViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TabBarViewController.swift
+//  LoginApp
+//
+//  Created by Artem Druzhinin on 09.10.2022.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let tabBarAppearance = UITabBarAppearance()
+        tabBarAppearance.configureWithOpaqueBackground()
+        tabBar.standardAppearance = tabBarAppearance
+        tabBar.scrollEdgeAppearance = tabBarAppearance
+    }
+
+}
